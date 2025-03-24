25c881cce9e861c107078ed8c1da75968e105c61
maysadasilva <maysapedro.01@hotmail.com>
2025-03-24 14:49:21 -0300

    Criando Constraints nas tabelas empresa.funcionario, departamento, departamento_localizacao, projeto, funcionario_projeto e dependente.

--- exemplobancodedados/empresa.sql
+++ exemplobancodedados/empresa.sql
@@ -1,5 +1,7 @@
 create database empresa;
 use empresa;
+drop database  empresa;
+drop table empresa.funcionario;
 create table empresa.funcionario(
 	Ssn char(9) not null,
     primeiro_nome varchar(20) not null,
@@ -14,37 +16,40 @@ create table empresa.funcionario(
 desc empresa.funcionario;
 
 create table departamento(
-    numero int not null primary key,
-    nome varchar(15) not null unique,
+    numero int not null,
+    nome varchar(15) not null,
     gerente_ssn char(9) not null,
     gerente_start_date date not null,
-    foreign key ( gerente_ssn) references empresa.funcionario (Ssn)
-    
+    constraint pk_departamento primary key (numero),
+    constraint unique_nome_deparment unique (nome),
+   constraint fk_departamento
+   foreign key ( gerente_ssn) references empresa.funcionario (Ssn)
 );
 
 create table departamento_localizacao(
    numero_departamento int not null,
    localizacao varchar(15) not null,
-   primary key (numero_departamento, localizacao),
-   foreign key  (numero_departamento) references departamento(numero)
+  constraint pk_num_departamento_local primary key (numero_departamento, localizacao),
+  constraint fk_num_depar foreign key  (numero_departamento) references departamento(numero)
 );
 
 create table projeto(
-    nome varchar(30) not null unique,
+    nome varchar(30) not null,
     numero_projeto int not null,
     projeto_localizacao varchar (15),
     num_departamento int not null,
-    primary key (numero_projeto),
-    foreign key (num_departamento) references departamento(numero)
+    constraint unique_nomeProjeto unique (nome),
+   constraint pk_num_proj primary key (numero_projeto),
+   constraint fk_num_depa foreign key (num_departamento) references departamento(numero)
 );
 
 create table funcionario_projeto(
     Essn char(5) not null,
     projeto_numero int not null,
     horas datetime not null,
-    primary key (Essn, projeto_numero),
-    foreign key (Essn) references empresa.funcionario (Ssn),
-    foreign key (projeto_numero) references projeto(numero_projeto) 
+   constraint pk_essn primary key (Essn, projeto_numero),
+   constraint fk_ess foreign key (Essn) references empresa.funcionario (Ssn),
+   constraint fk_pro_numero foreign key (projeto_numero) references projeto(numero_projeto) 
 );
 
 create table dependente(
@@ -53,6 +58,6 @@ create table dependente(
    sexo enum ('M','F'),
    data_nascimento date,
    relacionamento_empregado varchar(8),
-   primary key (Essn, dependente_nome),
-   foreign key (Essn) references empresa.funcionario(Ssn)
+   constraint pk_essn_dependente primary key (Essn, dependente_nome),
+  constraint fk_essn foreign key (Essn) references empresa.funcionario(Ssn)
 );
